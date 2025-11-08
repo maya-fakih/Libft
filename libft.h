b65c97613676b64fed75acd951e6359ef8067152
Maya Fakih <mfakih@c1r7s2.42beirut.com>
2025-11-08 17:49:52 +0200

    pushing header file

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfakih <mfakih@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 17:34:57 by mfakih            #+#    #+#             */
+/*   Updated: 2025/11/08 17:39:56 by mfakih           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+
+int	ft_atoi(const char *str);
+
+#endif
